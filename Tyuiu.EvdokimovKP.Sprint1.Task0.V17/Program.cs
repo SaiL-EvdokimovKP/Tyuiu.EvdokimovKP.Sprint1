17b20155936e2820b1ac9e46c6c4752347c8b093
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-24 01:49:23 +0500

    Add project files.

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task0.V17/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task0.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #17                                                              *");
+        Console.WriteLine("* Выполнил: Евдокимов Кирилл Петрович | ПКТб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение 9/3*2+12/4/3-2          *");
+        Console.WriteLine("* и печатает результат на экране.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* 9/3*2+12/4/3-2                                                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
