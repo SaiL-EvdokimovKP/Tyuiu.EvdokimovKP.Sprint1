b298c05f2fc10b8e89af8b24419fbed2a747ed1d
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-29 05:46:33 +0500

    Task2.V28

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task2.V28.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint1.Task2.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            var res = (int)(273 + x);
+            Assert.AreEqual(274, res);
+        }
+    }
+}
+
