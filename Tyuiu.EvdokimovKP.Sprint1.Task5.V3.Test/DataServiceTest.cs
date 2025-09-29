b93b752a948966e2d11496b337cb5526a02abf24
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-29 08:36:07 +0500

    Task5.V3

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int k = int.Parse("123456");
+            DataService ds = new DataService();
+            int h = ds.Calculate(k);
+            int result = Convert.ToInt32(h);
+            int wait = 4;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task5.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Евдокимов К. П. | ПКТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнил: Евдокимов Кирилл Петрович | ПКТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("*  вычисляет результат по формуле и печатает результат на экране.         *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+Console.WriteLine("***************************************************************************");
+
+int k;
+Console.WriteLine("Введите значение k");
+k = Convert.ToInt32(Console.ReadLine());
+
+int result = Convert.ToInt32((int) + ds.Calculate(k));
+Console.WriteLine(result);
+Console.ReadLine();
