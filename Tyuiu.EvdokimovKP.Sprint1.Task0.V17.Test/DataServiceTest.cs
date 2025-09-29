264f1b4debec93bd18aaf77c4915d48ff83e9881
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-29 05:38:08 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task0.V17.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint1.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+        }
+    }
+}
+
---
+++ Tyuiu.EvdokimovKP.Sprint1.Task0.V17/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task0.V17.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Евдокимов К. П. | ПКТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #17                                                             *");
+Console.WriteLine("* Выполнил: Евдокимов Кирилл Петрович | ПКТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+Console.WriteLine("* 9 / 3 * 2 + 12 / 4 / 3 - 2 и печатает результат на экране.              *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* 9 / 3 * 2 + 12 / 4 / 3 - 2                                              *");
+
+Console.WriteLine(ds.Calculate());
+Console.ReadLine();
