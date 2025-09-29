968cdf8ef0af143d09854f3496b7473e147ffa28
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-29 05:53:34 +0500

    Task3.V7

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double verst = 1.0668;
+            verst.ToString("F2");
+            var res = ds.VerstsToKilometers(verst);
+            Assert.AreEqual(106.68, res);
+        }
+    }
+}
+
---
+++ Tyuiu.EvdokimovKP.Sprint1.Task3.V7/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task3.V7.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнил: Евдокимов К. П. | ПКТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #7                                                              *");
+Console.WriteLine("* Выполнил: Евдокимов Кирилл Петрович | ПКТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая пересчета расстояния из верст в километры   *");
+Console.WriteLine("* и печатает результат на экране.                                         *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double verst;
+Console.WriteLine("Введите расстояние в верстах –>");
+verst = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(+ds.VerstsToKilometers(verst));
+Console.ReadLine();
