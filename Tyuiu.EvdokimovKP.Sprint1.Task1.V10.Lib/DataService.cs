ac6d38ca8ca1cc061df9b79e9e1f55ded1d9e493
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-09-29 05:41:39 +0500

    Task1.V10

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task1.V10.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.EvdokimovKP.Sprint1.Task1.V10.Lib
+{
+    public class DataService : ISprint1Task1V10
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x + y) / (1 + x);
+        }
+    }
+}
+
---
+++ Tyuiu.EvdokimovKP.Sprint1.Task1.V10/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task1.V10.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Евдокимов К. П. | ПКТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Евдокимов Кирилл Петрович | ПКТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет выражение (x + y) / (1 + x)       *");
+Console.WriteLine("* и печатает результат на экране.                                         *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* (x + y) / (1 + x)                                                       *");
+
+double x, y;
+Console.WriteLine("Введите значение X:");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y:");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.Calculate(x, y));
+
+Console.ReadLine();
