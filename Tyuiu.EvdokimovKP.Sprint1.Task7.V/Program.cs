7b9bab02896f1e6a531b6a7349dd659970e93099
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-10-03 21:20:05 +0500

    Task7.V18

---
+++ Tyuiu.EvdokimovKP.Sprint1.Task7.V/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task7.V18.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Евдокимов К. П. | ПКТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнил: Евдокимов Кирилл Петрович | ПКТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+Console.WriteLine("***************************************************************************");
+
+double x;
+Console.WriteLine("Введите значение X");
+x = Convert.ToDouble(Console.ReadLine());
+
+double y;
+Console.WriteLine("Введите значение Y");
+y = Convert.ToDouble(Console.ReadLine());
+
+double z = ds.Calculate(x, y);
+Console.WriteLine("z = " + z);
---
+++ Tyuiu.EvdokimovKP.Sprint1.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.EvdokimovKP.Sprint1.Task7.V18.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint1.Task7.V18.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double x = 0.5;
+            double y = 0.5;
+            DataService ds = new DataService();
+            double z = ds.Calculate(x, y);
+            double wait = 1.191;
+            Assert.AreEqual(wait, z);
+        }
+    }
+}
